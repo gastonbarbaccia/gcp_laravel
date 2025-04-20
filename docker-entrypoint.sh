7657d3f10d2e77b0e31b38d8ada8c70fc0959184
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-04-19 22:51:08 -0300

    prueba de secret

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 # Copiamos el archivo del secreto montado
+
+echo "Contenido de /secrets:"
+ls -la /secrets
+
+# Copiamos el archivo del secreto montado
 if [ -f /secrets/laravel_env ]; then
+  echo "Secreto encontrado, copiando a .env"
   cp /secrets/laravel_env /var/www/.env
+else
+  echo "Secreto NO encontrado en /secrets/laravel_env"
 fi
 
 # Ejecutamos el servidor embebido de Laravel
